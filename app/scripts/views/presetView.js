99a7878e2656a88f26b34248fdc5c7d8b1f70479
Sajjad Anwar <me@sajjad.in>
2014-06-12 13:29:51 +0530

    Added all fields and views. Managing all fields within a same view and login in templates. Much commit.

---
+++ app/scripts/views/presetView.js
@@ -0,0 +1,72 @@
+define([
+    'backbone',
+    'marionette',
+    'underscore',
+    'text!templates/presetTemplate._',
+    'app',
+    'models/field',
+    'views/fieldView',
+    'views/rawTagView'
+],
+
+function (Backbone, Marionette, _, addPresetTemplate, app, Field, FieldView, RawTagView) {
+
+    return Backbone.Marionette.Layout.extend({
+
+        template: _.template(addPresetTemplate),
+
+        regions: {
+            fields: '#fields'
+        },
+
+        events: {
+            'change #tagSelect': 'tagSelected'
+        },
+
+        ui: {
+            'tagSelect': '#tagSelect'
+        },
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+            console.log('add preset view');
+        },
+
+        tagSelected: function () {
+            var value = this.ui.tagSelect.val(),
+                fieldViewType;
+
+            if (value === 'tag') {
+
+                // Create a new raw tag view with the existing preset model.
+                var newRawTagView = new RawTagView({model: this.model});
+                app.modalRegion.show(newRawTagView);
+
+                return;
+            }
+
+            // Create a model based on the type of field.
+            var newFieldModel = new Field({type: value});
+
+            // Make this view listen to the model.
+            this.listenTo(newFieldModel, 'all', this.doSomething);
+
+            // Create a view
+            var newFieldView = new FieldView({
+                model: newFieldModel, 
+                presetModel: this.model,
+                // fieldViewType: fieldViewType
+            });
+
+            // var fieldView = new FieldView({model: newField});
+            app.modalRegion.show(newFieldView);
+
+            // console.log(newField);
+        },
+
+        doSomething: function () {
+            console.log('Something changed!');
+        }
+
+    });
+});
